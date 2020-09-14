de202993cffb18daa94a8fb3fb53b7837227e4d2
Konstantin <konstantin.kraynov@yandex.ru>
2020-09-14 23:10:01 +0300

    Добавлен CTFd

--- pages/rating.php
+++ pages/rating.php
@@ -1,6 +1,6 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <iframe style="height: calc(100vh - 7rem - 8px)" width="100%" hspace="0" frameborder="no" src="https://ctfd.dc78182.org/challenges">
+    <iframe style="height: calc(100vh - 7rem - 8px)" width="100%" hspace="0" frameborder="no" src="https://ctf.dc78182.org/users">
         Ваш браузер не поддерживает плавающие фреймы!
     </iframe>
 </div>
